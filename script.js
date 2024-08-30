1f8ed28d011f7d89854c965bcaf44b4da898d248
katie-soft <analyst.meo@gmail.com>
2024-08-30 22:14:07 +0300

    feat: add burger menu

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-// Photo gallery
+/* Burger menu */
+
+const burgerButton = document.querySelector('#burger-button');
+const closeBurgerIcon = document.querySelector('#close-burger');
+const burgerMenu = document.querySelector('#header-nav');
+const overlay = document.querySelector('#overlay');
+
+burgerButton.addEventListener('click', openBurgerMenu);
+closeBurgerIcon.addEventListener('click', closeBurgerMenu);
+
+function openBurgerMenu() {
+  burgerMenu.classList.add('burger-menu_opened');
+  overlay.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+}
+
+function closeBurgerMenu() {
+  burgerMenu.classList.remove('burger-menu_opened');
+  overlay.style.display = 'none';
+  document.body.style.overflow = 'auto';
+}
+
+/* Photo gallery */
 
 const swiper = new Swiper('.swiper', {
   loop: true,
