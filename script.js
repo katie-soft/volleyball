95335a07eab39fdfd7e9e02de276904620f79707
katie-soft <analyst.meo@gmail.com>
2024-08-31 19:40:27 +0300

    feat: add tabs to modal window

--- script.js
+++ script.js
@@ -38,3 +38,47 @@ const userModalCloseButton = modal.querySelector('#user-modal-close-button');
 
 userModalOpenButton.addEventListener('click', () => modal.showModal());
 userModalCloseButton.addEventListener('click', () => modal.close());
+
+const modalContentLogin = document.querySelector('.modal__login');
+const modalContentRegistration = document.querySelector('.modal__registration');
+const modalButtonLogin = document.querySelector('.button_login');
+const modalButtonsRegistration = document.querySelectorAll('.button_registration');
+
+modalButtonLogin.addEventListener('click', (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+  modalContentLogin.style.display = 'block';
+  modalContentRegistration.style.display = 'none';
+});
+
+modalButtonsRegistration.forEach(btn => btn.addEventListener('click', (event) => {
+  console.log(event.target)
+  event.stopPropagation();
+  event.preventDefault();
+  modalContentLogin.style.display = 'none';
+  modalContentRegistration.style.display = 'block';
+}));
+
+/* Login tabs */
+
+const tabButtonEmail = document.querySelector('#tab-button-email');
+const tabButtonPhone = document.querySelector('#tab-button-phone');
+const tabFormEmail = document.querySelector('#tab-form-email');
+const tabFormPhone = document.querySelector('#tab-form-phone');
+
+tabButtonEmail.addEventListener('click', openEmailTab);
+tabButtonPhone.addEventListener('click', openPhoneTab);
+
+function openEmailTab() {
+  tabFormPhone.classList.remove('modal__form_active');
+  tabFormEmail.classList.add('modal__form_active');
+  tabButtonEmail.classList.add('modal__tab_active');
+  tabButtonPhone.classList.remove('modal__tab_active');
+}
+
+function openPhoneTab() {
+  tabFormEmail.classList.remove('modal__form_active');
+  tabFormPhone.classList.add('modal__form_active');
+  tabButtonPhone.classList.add('modal__tab_active');
+  tabButtonEmail.classList.remove('modal__tab_active');
+}
